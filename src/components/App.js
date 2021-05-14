28699da21b3e0fe5810187dcffcaa00ea368c50a
vikasbagoria <vikasbagoria@gmail.com>
2021-05-14 17:46:05 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,29 @@ class App extends Component {
     constructor(props) {
 		super(props);
 		this.state = {
-			message:''
+			// message:''
+			isShow:false
 		}
-	}
+		this.fn= this.fn.bind(this);
+	};
 
-	clickHandler(){
-		this.setState({
-			message:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-		})
+	fn(){
+		this.setState({isShow:true})
 	}
+	// clickHandler(){
+	// 	this.setState({
+	// 		message:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+	// 	})
+	// }
 	
 
     render() {
     	return(
     		<div id="main">
-				<p id="para">{this.state.message}</p>
-					<button id="click" onClick={this.clickHandler.bind(this)}>Click me</button> 
+			<button id='click' onClick={this.fn}>Click me</button>
+      		{this.state.isShow &&<p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+				{/* <p id="para">{this.state.message}</p>
+					<button id="click" onClick={this.clickHandler.bind(this)}>Click me</button>  */}
     		</div>
     	);
     }
